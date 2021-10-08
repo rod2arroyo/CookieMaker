0439dfca33336bd177c8c2eefe8cbfc0e70e74b3
Eldrick Mnedoza <eldrickeldrickeldrick@gmail.com>
2021-10-08 00:20:10 -0500

    eeee

---
+++ app/src/main/java/com/example/cookiemaker/AddCookieActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cookiemaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddCookieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addcookie)
+
+
+    }
+}
